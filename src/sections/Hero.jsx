2ec715baba24eb241b334a17c7ca1c3f45bab7e2
saihemanth517 <saihemanthchoppadhandhi863@gmail.com>
2025-07-29 14:20:20 +0530

    Updated resume PDF and link

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             <div className="mt-4 hero-buttons">
               <Button
                 variant="outline-light"
-                href="https://drive.google.com/file/d/1x7_7-CTIP-xYYsDWtcvYCHh1saiDxhfd/view?usp=drivesdk"
+                href="https://drive.google.com/file/d/1-sSRO2_eEsDDbRkRn2-RUUogcHxoqDY2/view?usp=drivesdk"
                 className="me-3"
               >
                 My Resume
